07aed616a4369ac5eba5dda12a0dec2dfdd46036
sb <sb@action.ms>
2022-06-16 18:11:03 +0200

    Improved directory traversal. Action can be IO action.

--- app/traverse/Main.hs
+++ app/traverse/Main.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE TypeApplications #-}
+-- |
+-- Traverse directory hierarchy.
+-- Effective Haskell ch. 10
+--
 module Main where
 
 import Control.Exception (IOException, handle)
@@ -46,18 +50,18 @@ naiveTraversal rootPath action = do
 
 -- |
 -- More efficient directory traversal handling also circular references.
-traverseDirectory :: FilePath -> (FilePath -> a) -> IO [a]
+traverseDirectory :: FilePath -> (FilePath -> IO ()) -> IO ()
 traverseDirectory rootPath action = do
-    -- Keep all paths already seen in a set.
+    -- Keep reference to all paths already seen in a set.
     seenRef <- newIORef Set.empty
-    -- Keep a list of results.
-    resultRef <- newIORef []
     let
         -- Some helper functions that need access to seenRef
         haveSeenDirectory canonicalPath =
             Set.member canonicalPath <$> readIORef seenRef
+
         addDirectoryToSeen canonicalPath = do
             modifyIORef seenRef $ Set.insert canonicalPath
+
         traverseSubdirectory subdirPath = do
             contents <- listDirectory subdirPath
             for_ contents $ \file' ->
@@ -67,21 +71,26 @@ traverseDirectory rootPath action = do
                     classification <- classifyFile canonicalPath
                     case classification of
                       FileTypeOther -> pure ()
-                      FileTypeRegularFile -> modifyIORef resultRef (\results -> action file : results)
+                      FileTypeRegularFile -> action file
                       FileTypeDirectory -> do
                         alreadyProcessed <- haveSeenDirectory file
                         when (not alreadyProcessed) $ do
                           addDirectoryToSeen file
                           traverseSubdirectory file
     traverseSubdirectory (dropSuffix "/" rootPath)
-    readIORef resultRef
 
 
-
+traverseDirectory' :: FilePath -> (FilePath -> a) -> IO [a]
+traverseDirectory' rootPath action = do
+    -- Keep reference to list of result items.
+    resultsRef <- newIORef []
+    traverseDirectory rootPath $ \file -> do
+        modifyIORef resultsRef (action file :)
+    readIORef resultsRef
 
 
 main = do
-    files <- traverseDirectory "./" (\p -> p)
+    files <- traverseDirectory' "./" (\p -> p)
     mapM putStrLn files
     putStrLn $ "Number of items: " <> show (length files)
     return ()
