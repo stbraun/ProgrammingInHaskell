edbe6f9deaa6ac8894f5c00020f9125d5ea3ed09
sb <sb@action.ms>
2022-06-25 21:30:50 +0200

    Using callCommand instead of deprecated runCommand.

--- src/Sound.hs
+++ src/Sound.hs
@@ -2,6 +2,7 @@ module Sound where
 
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
+import qualified Control.Concurrent as C
 import Data.Foldable
 import Text.Printf
 import System.Process
@@ -34,12 +35,12 @@ f :: Semitones -> Frequency
 f n = pitchStandard * (2**(1.0/12.0))**n
 
 
--- Create samples for a note n semi tones from pitch standard.
+-- | Create samples for a note n semi tones from pitch standard.
 note :: Semitones -> Duration -> [Sample]
 note n duration = tone (f n) duration
 
 
--- Create samples for a tone of given frequency and duration.
+-- | Create samples for a tone of given frequency and duration.
 tone :: Frequency -> Duration -> [Sample]
 tone frequency duration = map (* volume) $ map sin $ map (* step) [0.0..sampleRate * duration]
     where
@@ -50,14 +51,22 @@ save :: FilePath -> [Sample] -> IO ()
 save filePath samples = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE samples
 
 
+-- | Play samples as audio.
 play :: [Sample] -> IO ()
 play samples = do
     save outputFilePath samples
-    _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
-    return ()
+    callCommand $ printf "ffplay -f f32le -ar %f -t %d %s" sampleRate duration outputFilePath
+    putStrLn $ printf "Duration [s]: %f / %d" (soundDuration samples) duration
+    where
+        duration :: Int
+        duration = (ceiling $ soundDuration samples) -- * 10**6
+
+-- | Cumulated duration of samples.
+soundDuration :: [Sample] -> Duration
+soundDuration samples = (fromIntegral (length samples)) / sampleRate
 
 
--- Compose a series of tones.
+-- | Compose a series of tones for test purposes.
 testWave :: [Sample]
 testWave = concat [ note 0 0.5
               , note 2 0.25
