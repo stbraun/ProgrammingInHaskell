d144bd20a9f3dd28e7c5632d95ea576b9f126a22
sb <sb@action.ms>
2022-07-24 19:16:49 +0200

    Collatz function added.

--- src/Excurs.hs
+++ src/Excurs.hs
@@ -1,5 +1,15 @@
 module Excurs where
 
+import qualified Data.Text as Text
+import Text.Printf
+
+
+-- safe div
+sdiv :: Maybe Double -> Maybe Double -> Maybe Double
+sdiv Nothing _ = Nothing
+sdiv _ Nothing = Nothing
+sdiv (Just n) (Just m) = if m==0 then Nothing else Just (n / m)
+
 
 -- safe add
 sadd :: Num a => Maybe a -> Maybe a -> Maybe a
@@ -14,14 +24,14 @@ sadd (Just x) (Just y) = Just (x + y)
 -- hours duration over which leak was observed
 -- operations per second - throughput during test
 -- returns leak in Byte/operation
-leakPerOp :: Float -> Float -> Float -> Float
+leakPerOp :: Double -> Double -> Double -> Double
 leakPerOp leak hours operations = leak * 10^6 / (operations * hours * 3600)
 
 
 data LeakStat = LeakStat
-    { perHour :: Float
-    , perDay :: Float
-    , perWeek :: Float
+    { perHour :: Double
+    , perDay :: Double
+    , perWeek :: Double
     }
 
 -- |
@@ -30,29 +40,34 @@ data LeakStat = LeakStat
 -- hours duration over which leak was observed
 -- operations per second - throughput during test
 -- returns leak in MB per (hour, day, week)
-leakOverTime :: Float -> Float -> LeakStat
+leakOverTime :: Double -> Double -> LeakStat
 leakOverTime leak hours = LeakStat hour day week
     where
         hour = leak / hours
         day  = hour * 24
         week = day * 7
 
-reportOnLeak :: Float -> Float -> Float -> IO ()
+reportOnLeak :: Double -> Double -> Double -> IO ()
 reportOnLeak leak hours operations = do
     putStrLn "--- Memory Leak ---"
-    putStrLn  $ "Leak of " ++ (show leak) ++ "MB/"
-                ++ (show hours) ++ "h at " ++ (show operations) ++ "ops/s"
-    putStrLn $ "Operations per day: " ++ (show (operations * 3600 * 24))
-    putStr "Leak per op:   "
-    putStrLn $ show (leakPerOp leak hours operations) ++ "B"
-    putStr "Leak per hour:  "
-    putStrLn $ show (perHour leakStats) ++ "MB"
-    putStr "Leak per day:  "
-    putStrLn $ show (perDay leakStats) ++ "MB"
-    putStr "Leak per week: "
-    putStrLn $ show (perWeek leakStats) ++ "MB"
+    printf "Leak of %.2fMB over %.1fh at %.1fops/s\n" leak hours operations
+    printf "Operations per day: %.0f\n" (operations * 3600 * 24)
+    printf "Leak per op:   %7.1f B\n" $ leakPerOp leak hours operations
+    printf "Leak per hour: %7.2f MB\n" (perHour leakStats)
+    printf "Leak per day:  %7.2f MB\n" (perDay leakStats)
+    printf "Leak per week: %7.2f MB\n" (perWeek leakStats)
     where
         leakStats = leakOverTime leak hours
+
+evalLeak :: [Double] -> [Double] -> Double -> IO ()
+evalLeak privbytes hours opsPerSec = reportOnLeak leak duration opsPerSec
+    where
+        leak = (head . reverse) privbytes - (head privbytes)
+        duration = (head . reverse) hours - (head hours)
+
+
+durations = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 36] :: [Double]
+privbytes = [200.5, 201.0, 201.4, 202.0, 202.3, 202.6, 202.9, 203.7, 204.0, 204.3, 204.6, 205.0, 207.2, 209.1] :: [Double]
 
 
 -- | Logistic map.
@@ -64,4 +79,21 @@ reportOnLeak leak hours operations = do
 logMap :: Double -> Double -> Int -> [Double]
 logMap r xt 0 = [xt]
 logMap r xt n = xt : logMap r (r * xt * (1 - xt)) (n - 1)
+
+
+-- Collatz Conjecture
+collatz' :: Int -> Int -> Int
+collatz' 1 i = i
+collatz' n i
+    | n < 1 = error "Input must be > 0"
+    | even n = collatz' (n `div` 2) (i +1)
+    | otherwise = collatz' ( (3 * n + 1) `div` 2) (i + 1)
+    where even n = n `mod` 2 == 0
+
+collatz :: Int -> IO ()
+collatz n = let iterations = collatz' n 0
+         in printf "collatz(%d) terminated after %d iterations\n" n iterations
+
+coll :: [Int] -> IO ()
+coll ns = putStrLn $ (Text.unpack . Text.intercalate (Text.pack "\n") . map Text.pack . map show)  [(n, collatz' n 0) | n <- ns]
 
