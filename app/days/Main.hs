c00530d8be4b7847fed2f74f7e938ad7be4f39f3
sb <sb@action.ms>
2022-09-02 18:11:32 +0200

    Subtract weekend days.

--- app/days/Main.hs
+++ app/days/Main.hs
@@ -1,6 +1,5 @@
 -- | Count days to a given date.
 --
-
 module Main where
 
 import qualified Data.Time.Calendar as Cal
@@ -12,6 +11,11 @@ main =  do
     now <- Cl.getCurrentTime
     let end = Cal.fromGregorian 2024 10 31
         today = Cl.utctDay now
+        diffDays = Cal.diffDays end today
 
-    printf "Days from %s to %s -> %d\n" (show today) (show end) (Cal.diffDays end today)
+    printf "Days from %s to %s -> %d (work days %d)\n" (show today) (show end) diffDays (workDays diffDays)
+
+-- | Approximate number of weekend days for a given span of days.
+workDays :: Integer -> Integer
+workDays days = days * 5 `div` 7
 
