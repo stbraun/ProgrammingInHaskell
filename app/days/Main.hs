05605c9e567e6e44e2d33c199bfcfdfd21087c04
sb <sb@stbraun.com>
2022-12-01 00:54:07 +0000

    Days takes holidays in consideration.

--- app/days/Main.hs
+++ app/days/Main.hs
@@ -19,8 +19,9 @@ days end = do
         diffDays = Cal.diffDays end today
         remWorkDays = workDays diffDays
         remVacancies = remWorkDays - vacationDays
+        remHolidays = remVacancies - holidays
 
-    printf "Days from %s to %s -> %d (work days: %d) (after vacancies: %d)\n" (show today) (show end) diffDays remWorkDays remVacancies
+    printf "Days from %s to %s -> %d (work days: %d) (after vacancies and holidays: %d)\n" (show today) (show end) diffDays remWorkDays remHolidays
 
 -- | Approximate number of workdays for a given span of days.
 workDays :: Integer -> Integer
@@ -34,3 +35,11 @@ vacationDays = y2024 + y2023 + y2022
         y2023 = 30
         y2024 = 10 * 30 `div` 12
 
+-- | Approximate number of holidays
+holidays :: Integer
+holidays = y2022 + y2023 + y2024
+    where
+        y2022 = 1
+        y2023 = 10
+        y2024 = 5
+ 
