207e5afc0d67412b4850d20e85a4652f14396ffc
Stefan Braun <sb@stbraun.com>
2024-09-29 12:29:03 +0200

    Cosmetic changes.

--- app/days/Main.hs
+++ app/days/Main.hs
@@ -15,13 +15,21 @@ lastDay :: Cal.Day
 lastDay = dayBeforeRetirement
 --lastDay = Cal.YearMonthDay 2025 10 31
 
--- | Configure the list of planned vacation days.
+-- | Provide the list of planned vacation days.
 plannedVacationDays :: [Cal.Day]
 plannedVacationDays = [toDate 2024 05 27, toDate 2024 05 28, toDate 2024 05 29, toDate 2024 05 31,
                        toDate 2024 07 01, toDate 2024 07 02, toDate 2024 07 03, toDate 2024 07 04, toDate 2024 07 05,
                        toDate 2024 07 08, toDate 2024 07 09, toDate 2024 07 10, toDate 2024 07 11, toDate 2024 07 12,
                        toDate 2024 09 09, toDate 2024 09 10, toDate 2024 09 11, toDate 2024 09 12, toDate 2024 09 13,
                        toDate 2024 09 30, toDate 2024 10 01, toDate 2024 10 02]
+
+-- | Provide the list of holidays.
+holidaysList :: [Cal.Day]
+holidaysList = [toDate 2024 5 1, toDate 2024 5 9, toDate 2024 5 20, toDate 2024 5 30, toDate 2024 10 3,
+                toDate 2024 12 24, toDate 2024 12 25, toDate 2024 12 26, toDate 2024 12 31,
+                toDate 2025 01 01, toDate 2025 03 03, toDate 2025 04 18, toDate 2025 04 21, toDate 2025 05 01,
+                toDate 2025 05 29, toDate 2025 06 09, toDate 2025 06 19, toDate 2025 10 03, toDate 2025 11 01,
+                toDate 2025 12 24, toDate 2025 12 25, toDate 2025 12 26, toDate 2025 12 31]
 
 
 -- | Calculate remaining days and print a report.
@@ -52,14 +60,6 @@ days first last
 -- | Provide the current day.
 today :: IO Cal.Day
 today = fmap Cl.utctDay Cl.getCurrentTime
-
--- | Provide a list of holidays.
-holidaysList :: [Cal.Day]
-holidaysList = [toDate 2024 5 1, toDate 2024 5 9, toDate 2024 5 20, toDate 2024 5 30, toDate 2024 10 3,
-                toDate 2024 12 24, toDate 2024 12 25, toDate 2024 12 26, toDate 2024 12 31,
-                toDate 2025 01 01, toDate 2025 03 03, toDate 2025 04 18, toDate 2025 04 21, toDate 2025 05 01,
-                toDate 2025 05 29, toDate 2025 06 09, toDate 2025 06 19, toDate 2025 10 03, toDate 2025 11 01,
-                toDate 2025 12 24, toDate 2025 12 25, toDate 2025 12 26, toDate 2025 12 31]
 
 -- | Number of open vacation days.
 numVacationDays :: Cal.Day -> Int
