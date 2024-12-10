ce0ce1e1506cf8c34fea218e85a9c88dfd14730d
Stefan Braun <sb@stbraun.com>
2024-12-10 07:37:02 +0100

    Vacation days added.

--- app/days/Main.hs
+++ app/days/Main.hs
@@ -26,7 +26,8 @@ plannedVacationDays = [toDate 2024 05 27, toDate 2024 05 28, toDate 2024 05 29, 
                        toDate 2024 07 08, toDate 2024 07 09, toDate 2024 07 10, toDate 2024 07 11, toDate 2024 07 12,
                        toDate 2024 09 09, toDate 2024 09 10, toDate 2024 09 11, toDate 2024 09 12, toDate 2024 09 13,
                        toDate 2024 09 30, toDate 2024 10 01, toDate 2024 10 02,
-                       toDate 2024 10 23, toDate 2024 10 24, toDate 2024 10 25]
+                       toDate 2024 10 23, toDate 2024 10 24, toDate 2024 10 25,
+                       toDate 2024 12 23, toDate 2024 12 30]
 
 -- | Provide the list of holidays.
 holidaysList :: [Cal.Day]
