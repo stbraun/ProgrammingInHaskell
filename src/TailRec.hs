f4e00b8048859c738aaedf997a1aa43004f339fa
Stefan Braun <sb@stbraun.com>
2025-03-28 16:13:06 +0100

    Minor improvements to TailRec.hs.

--- src/TailRec.hs
+++ src/TailRec.hs
@@ -3,16 +3,22 @@ module TailRec where
 
 -- | Fibonacci function using tail recursion
 fib :: Integer -> Integer
-fib n = go n 0 1
+fib n = fib' n 0 1
   where
-    go 0 a _ = a
-    go 1 _ b = b
-    go n a b = go (n-1) b (a+b)
+    -- | Helper function for fib taking two additional arguments
+    -- | to store the previous two Fibonacci numbers.
+    fib' :: Integer -> Integer -> Integer -> Integer
+    fib' 0 a _ = a
+    fib' 1 _ b = b
+    fib' n a b = fib' (n-1) b (a+b)
 
 -- | Factorial function using tail recursion
 fact :: Integer -> Integer
-fact n = go n 1
+fact n = fact' n 1
   where
-    go 0 a = a
-    go n a = go (n-1) (n*a)
+    -- | Helper function for fact taking an additional argument
+    -- | to store the current factorial value.
+    fact' :: Integer -> Integer -> Integer
+    fact' 0 acc = acc
+    fact' n acc = fact' (n-1) (n*acc)
 
